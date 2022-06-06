8c16aa4f7d12d7f892e7237d6ca8eefe29139df1
Deep Zalavadiya <deepzalavadiya2001@gmail.com>
2022-06-06 11:18:00 +0530

    Updates

---
+++ precache-manifest.97e2ab05e3494fae8e14e4590fb5eb25.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "539966aad0682af5c2cd",
+    "url": "/react-web-app2/static/css/main.f2b6ef29.chunk.css"
+  },
+  {
+    "revision": "539966aad0682af5c2cd",
+    "url": "/react-web-app2/static/js/main.539966aa.chunk.js"
+  },
+  {
+    "revision": "546dafc18c5050b214b1",
+    "url": "/react-web-app2/static/js/1.546dafc1.chunk.js"
+  },
+  {
+    "revision": "04f038a231a0522aa91d",
+    "url": "/react-web-app2/static/css/2.fff00666.chunk.css"
+  },
+  {
+    "revision": "04f038a231a0522aa91d",
+    "url": "/react-web-app2/static/js/2.04f038a2.chunk.js"
+  },
+  {
+    "revision": "80b9c08ebb9987bbe040",
+    "url": "/react-web-app2/static/js/runtime~main.80b9c08e.js"
+  },
+  {
+    "revision": "8b662c065ff6a90b87870a020b4b2c88",
+    "url": "/react-web-app2/static/media/Rectangle 7.8b662c06.svg"
+  },
+  {
+    "revision": "25e65df897f342301d97cff915433b31",
+    "url": "/react-web-app2/static/media/Group 2.25e65df8.svg"
+  },
+  {
+    "revision": "268ee796af94c6c648f374962200c9a8",
+    "url": "/react-web-app2/static/media/PngItem_4906041 1.268ee796.svg"
+  },
+  {
+    "revision": "c1d5b032629ab765a6dbe5ace467d941",
+    "url": "/react-web-app2/static/media/Rectangle 7(1).c1d5b032.svg"
+  },
+  {
+    "revision": "1a306af5941749c8e1786f977e2771e8",
+    "url": "/react-web-app2/static/media/Rectangle 7 (2).1a306af5.svg"
+  },
+  {
+    "revision": "b8a7422ff4c1605f69f7aba041bfe9c9",
+    "url": "/react-web-app2/static/media/Rectangle 15.b8a7422f.svg"
+  },
+  {
+    "revision": "95ca8443cdb92105c60848eddb285358",
+    "url": "/react-web-app2/static/media/search_small.95ca8443.svg"
+  },
+  {
+    "revision": "476d9b4537822b36e301368ef825d8ec",
+    "url": "/react-web-app2/static/media/Vector 1.476d9b45.svg"
+  },
+  {
+    "revision": "36f22bf9ff87fe4e5fc2e71e64d4b274",
+    "url": "/react-web-app2/static/media/Vector.36f22bf9.svg"
+  },
+  {
+    "revision": "eeaf53686bb05a34520eab7ca30f7624",
+    "url": "/react-web-app2/static/media/Group 7.eeaf5368.svg"
+  },
+  {
+    "revision": "e59350302b34817945c147624b56c9e3",
+    "url": "/react-web-app2/static/media/Group 3.e5935030.svg"
+  },
+  {
+    "revision": "f6290c2224b050769946c5e018c1ae21",
+    "url": "/react-web-app2/static/media/Group 9.f6290c22.svg"
+  },
+  {
+    "revision": "e4b400fe1d135d56322f93f6f978dbc7",
+    "url": "/react-web-app2/static/media/Group 10.e4b400fe.svg"
+  },
+  {
+    "revision": "aedac1d3115d9c765af4f5458d619a72",
+    "url": "/react-web-app2/static/media/Shop.aedac1d3.svg"
+  },
+  {
+    "revision": "e3e44ab00ba75f2b7ef0dd5b5c2b86eb",
+    "url": "/react-web-app2/static/media/Group 37.e3e44ab0.svg"
+  },
+  {
+    "revision": "29b05ae5065d7c3712627b7eaa683723",
+    "url": "/react-web-app2/static/media/image 3.29b05ae5.svg"
+  },
+  {
+    "revision": "d9a29740ed69a08060362815ace16e7d",
+    "url": "/react-web-app2/static/media/Group 13.d9a29740.svg"
+  },
+  {
+    "revision": "83fcf62ac864bf4281fc7a4380e938b6",
+    "url": "/react-web-app2/static/media/Group 21.83fcf62a.svg"
+  },
+  {
+    "revision": "88c9f8f7587941dd0751e2cd07d6709a",
+    "url": "/react-web-app2/static/media/Group 22.88c9f8f7.svg"
+  },
+  {
+    "revision": "a61d39b3f1a35b7abeffdacdd08c457e",
+    "url": "/react-web-app2/static/media/image 1.a61d39b3.svg"
+  },
+  {
+    "revision": "0268d19ff3fbfcc61521fd319457ceeb",
+    "url": "/react-web-app2/index.html"
+  }
+];
